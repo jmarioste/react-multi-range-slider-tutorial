1393913e61c967c9b144a74b095ace33dfcab9dc
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-17 18:49:33 +0800

    Full code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
-
+import RangeSlider from "../components/RangeSlider";
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">
-        Welcome to react multi-range-slider tutorial
-      </h1>
+    <div className="flex flex-col gap-2 p-4">
+      <RangeSlider
+        className="w-80 h-12"
+        defaultValue={50}
+        marks={10}
+        step={10}
+      />
+      <RangeSlider className="w-40 h-8" defaultValue={[25, 75]} />
+      <RangeSlider
+        className="w-8 h-40"
+        orientation="vertical"
+        defaultValue={50}
+      />
     </div>
   );
 };
-
 export default Home;

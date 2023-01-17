f6d93281458b2394a65336ccd164594aa00461dc
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-17 17:41:18 +0800

    Change homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold ">
+        Welcome to react multi-range-slider tutorial
+      </h1>
     </div>
   );
 };
